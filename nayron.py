3443c70dd180172eba1d7c08848448b130e00762
Densuki <DarkShadow3315@gmail.com>
2018-10-03 20:38:59 -0300

    arrumando erro

--- nayron.py
+++ nayron.py
@@ -74,8 +74,8 @@ async def on_message(message): #Condição
     #if message.content.lower().startswith('#comandos'):
     #    await client.send_message(message.channel,"**# = Prefix.** `help, test`. **Sem prefix** `invite, fazer oq, aspectos, ygopro, mediafire, ygoprolink, tururu`")
 
-    if message.content.lower().startswith('invite'):
-        await client.send_message(message.channel,"https://discord.gg/pjRSBK4")	
+    #if message.content.lower().startswith('invite'):
+    #    await client.send_message(message.channel,"https://discord.gg/pjRSBK4")	
     if message.content.lower().startswith('#help'):
         await client.send_message(message.channel,"**NÃO SETARAM NADA NESTE COMANDO NÃO ANIMAL!!**")	
 
